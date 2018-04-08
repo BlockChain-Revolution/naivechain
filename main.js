4d1e9109ba9131b87710c966c7408441ac0a37ee
sunnode <m18205253820@163.com>
2018-04-08 14:25:25 +0800

    docs: 增加注释

--- main.js
+++ main.js
@@ -8,7 +8,17 @@ var http_port = process.env.HTTP_PORT || 3001;
 var p2p_port = process.env.P2P_PORT || 6001;
 var initialPeers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 
+// 体系结构
+// 需要指出的是，节点实际上展现了两个web服务器：
+// 一个（HTTP服务器）是让用户控制节点，
+// 另一个（Websocket HTTP服务器）
 class Block {
+    // 第一个逻辑步骤是决定块结构。
+    // 为了保证事情尽可能的简单，我们只选择最必要的部分：
+    // index（下标）
+    // timestamp（时间戳）
+    // data（数据）、hash（哈希值）
+    // previous hash（前置哈希值）
     constructor(index, previousHash, timestamp, data, hash) {
         this.index = index;
         this.previousHash = previousHash.toString();
@@ -20,17 +30,26 @@ class Block {
 
 var sockets = [];
 var MessageType = {
-    QUERY_LATEST: 0,
-    QUERY_ALL: 1,
-    RESPONSE_BLOCKCHAIN: 2
+    QUERY_LATEST: 0, // 获取最新
+    QUERY_ALL: 1, // 获取所有
+    RESPONSE_BLOCKCHAIN: 2 // 从区块链中获取
 };
 
+// 块的存储
+// 内存中的Javascript数组被用于存储区块链。区块链的第一个块通常被称为“起源块”，是硬编码的
 var getGenesisBlock = () => {
     return new Block(0, "0", 1465154705, "my genesis block!!", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
 };
 
 var blockchain = [getGenesisBlock()];
 
+// 结点控制
+// 在某种程度上用户必须能够控制结点。这一点通过搭建一个HTTP服务器可以实现
+// 用户可以用下面的方法和结点互动：
+// 列出所有的块 curl http://localhost:3001/blocks
+// 用用户提供的内容创建一个新的块 curl -H "Content-type:application/json" --data '{"data" : "Some data to the first block"}' http://localhost:3001/mineBlock
+// 列出或者新增peers curl -H "Content-type:application/json" --data '{"peer" : "ws://localhost:6001"}' http://localhost:3001/addPeer
+// 获取所有已连接的节点 curl http://localhost:3001/peers
 var initHttpServer = () => {
     var app = express();
     app.use(bodyParser.json());
@@ -53,7 +72,7 @@ var initHttpServer = () => {
     app.listen(http_port, () => console.log('Listening http on port: ' + http_port));
 };
 
-
+// 初始化P2P网络，用来接收块的信息
 var initP2PServer = () => {
     var server = new WebSocket.Server({port: p2p_port});
     server.on('connection', ws => initConnection(ws));
@@ -95,7 +114,10 @@ var initErrorHandler = (ws) => {
     ws.on('error', () => closeConnection(ws));
 };
 
-
+// 块的生成
+// 要生成一个块，必须知道前一个块的哈希值
+// 然后创造其余所需的内容（= index, hash, data and timestamp）
+// 块的data部分是由终端用户所提供的
 var generateNextBlock = (blockData) => {
     var previousBlock = getLatestBlock();
     var nextIndex = previousBlock.index + 1;
@@ -109,6 +131,9 @@ var calculateHashForBlock = (block) => {
     return calculateHash(block.index, block.previousHash, block.timestamp, block.data);
 };
 
+// 块哈希
+// 为了保存完整的数据，必须哈希区块。
+// SHA-256会对块的内容进行加密，记录这个值应该和“挖矿”毫无关系，因为这里不需要解决工作量证明的问题
 var calculateHash = (index, previousHash, timestamp, data) => {
     return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 };
@@ -119,6 +144,9 @@ var addBlock = (newBlock) => {
     }
 };
 
+// 确认块的完整性
+// 在任何时候都必须能确认一个区块或者一整条链的区块是否完整。
+// 在我们从其他节点接收到新的区块，并需要决定接受或拒绝它们时，这一点尤为重要。
 var isValidNewBlock = (newBlock, previousBlock) => {
     if (previousBlock.index + 1 !== newBlock.index) {
         console.log('invalid index');
@@ -166,6 +194,9 @@ var handleBlockchainResponse = (message) => {
     }
 };
 
+// 选择最长的链
+// 任何时候在链中都应该只有一组明确的块。万一冲突了（例如：两个结点都生成了72号块时）
+// 会选择有最大数目的块的链
 var replaceChain = (newBlocks) => {
     if (isValidChain(newBlocks) && newBlocks.length > blockchain.length) {
         console.log('Received blockchain is valid. Replacing current blockchain with received blockchain');
